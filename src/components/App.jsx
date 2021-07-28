c0973e4fcc3249cbc5bb25b823337fe9b4e11094
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2021-07-28 18:38:04 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createCard(emojiTerm) {
+  return (
+    <Card
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      meaning={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
